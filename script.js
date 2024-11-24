ea88e6f09bacca59e6b41da54c699d04eb97edbf
MateyNedyalkov221889 <112861226+MateyNedyalkov221889@users.noreply.github.com>
2024-11-24 17:34:53 +0100

    small updates

--- script.js
+++ script.js
@@ -4,3 +4,23 @@ function toggleMenu() {
     menu.classList.toggle("open")
     icon.classList.toggle("open")
 }
+
+// Darkmode
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const toggleButton = document.getElementById('dark-mode-toggle');
+//     toggleButton.addEventListener('click', () => {
+//         document.body.classList.toggle('dark-mode');
+//         let mode = 'disabled';
+//         if (document.body.classList.contains('dark-mode')) {
+//             mode = 'enabled';
+//         }
+//         localStorage.setItem('dark-mode', mode);
+//     });
+
+//     // Load the user's preference from localStorage
+//     const currentMode = localStorage.getItem('dark-mode');
+//     if (currentMode === 'enabled') {
+//         document.body.classList.add('dark-mode');
+//     }
+// });
